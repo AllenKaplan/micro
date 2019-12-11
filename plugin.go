c0606a3b4abc6b12a174d84390f88e90a3771f56
AllenKaplan <allenkaplan07@gmail.com>
2019-12-11 09:39:48 -0500

    CORS plugin.go

---
+++ plugin.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	// Flags usage of cors plugin `micro --cors-allowed-headers=X-Custom-Header --cors-allowed-origins=someotherdomain.com  --cors-allowed-methods=POST`
+	"github.com/micro/go-plugins/micro/cors"
+	"github.com/micro/micro/plugin"
+)
+
+func init() {
+	// lets not hard-code kubernetes, so that we can use same `micro` binary with consul
+	// set values for registry/selector
+	// os.Setenv("MICRO_REGISTRY", "kubernetes")
+	// os.Setenv("MICRO_SELECTOR", "static")
+
+	// setup cors plugin
+	plugin.Register(cors.NewPlugin())
+}
